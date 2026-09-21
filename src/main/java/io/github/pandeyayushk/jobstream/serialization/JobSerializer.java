600baa12f713b17f795d89218f1cad070778db44
Ayush Kumar Pandey <pandeyayushkumar1232007@gmail.com>
2026-09-21 18:24:41 +0530

    feat: add JobSerializer interface and SerializationException class for serialization support

---
+++ src/main/java/io/github/pandeyayushk/jobstream/serialization/JobSerializer.java
@@ -0,0 +1,4 @@
+package io.github.pandeyayushk.jobstream.serialization;
+
+public interface JobSerializer {
+}
---
+++ src/main/java/io/github/pandeyayushk/jobstream/serialization/SerializationException.java
@@ -0,0 +1,7 @@
+package io.github.pandeyayushk.jobstream.serialization;
+
+public class SerializationException extends RuntimeException {
+  public SerializationException(String message) {
+    super(message);
+  }
+}
